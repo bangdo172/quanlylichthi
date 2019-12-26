8ec91963bcd5a03e7294ea637a9d483f2ea0a20b
dean <huy99ls01@gmail.com>
2019-12-26 18:25:21 +0700

    example api

--- React_FrontEND/coreui-free-react-admin-template/src/views/Base/Cards/Cards.js
+++ React_FrontEND/coreui-free-react-admin-template/src/views/Base/Cards/Cards.js
@@ -1,4 +1,5 @@
 import React, { useState, Component } from 'react';
+import axios from 'axios';
 import { ButtonGroup, ButtonToolbar, Button, Badge, Card, CardBody, CardFooter, CardHeader, Col, Row, Collapse, Fade } from 'reactstrap';
 import { AppSwitch } from '@coreui/react';
 import Modal from 'react-bootstrap/Modal';
@@ -20,6 +21,8 @@ import {
 
 
 } from 'reactstrap';
+import { Route, Router, NavLink, HashRouter, BrowserRouter } from "react-router-dom";
+
 
 function Neww4(props) {
 
@@ -88,6 +91,12 @@ function Neww5(props) {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+
+
+  const goHome = () => {
+    this.props.history.push('./views/Widgets/Widgets')
+  }
+
   return (
     <>
       <Card>
@@ -99,7 +108,7 @@ function Neww5(props) {
               </CardBody>
         <CardFooter>
           <ButtonGroup aria-label="Basic example">
-            <Button variant="secondary" color="primary"><i className="fa fa-align-justify"></i> Quản lý lịch</Button>
+            <Button variant="secondary" color="primary" onClick={goHome}><i className="fa fa-align-justify"></i> Quản lý lịch</Button>
             <Button variant="secondary" color="warning" onClick={handleShow}><i className="fa fa-dot-circle-o"></i> Sửa đổi</Button>
             <Button variant="secondary" color="danger"><i className="fa fa-ban"></i> Xóa lịch</Button>
           </ButtonGroup>
@@ -137,6 +146,70 @@ function Neww5(props) {
 
 }
 
+
+function Funcc(props) {
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+
+      {
+        props.persons.map((p) => {
+          return (
+            <Card>
+              <CardHeader>
+                {p}
+              </CardHeader>
+              <CardBody>
+                Số môn thi: 4
+              </CardBody>
+              <CardFooter>
+                <ButtonGroup aria-label="Basic example">
+                  <Button variant="secondary" color="primary"><i className="fa fa-align-justify"></i> Quản lý lịch</Button>
+                  <Button variant="secondary" color="warning" onClick={handleShow}><i className="fa fa-dot-circle-o"></i> Sửa đổi</Button>
+                  <Button variant="secondary" color="danger"><i className="fa fa-ban"></i> Xóa lịch</Button>
+                </ButtonGroup>
+              </CardFooter>
+            </Card>
+
+          )
+        })
+      }
+
+      < Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Thêm kỳ thi mới</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Row>
+            <Col xs="12">
+              <FormGroup>
+                <Label htmlFor="name">Sửa tên kỳ thi:</Label>
+                <Input type="text" id="makhoahoc" placeholder="Tên kỳ thi" required />
+              </FormGroup>
+            </Col>
+          </Row>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose} color="danger">
+            <i className="fa fa-ban"></i>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose} color="primary">
+            <i className="fa fa-dot-circle-o"></i>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  )
+}
+
+
 class Cards extends Component {
   constructor(props) {
     super(props);
@@ -146,8 +219,23 @@ class Cards extends Component {
     this.state = {
       collapse: true,
       fadeIn: true,
-      timeout: 300
+      timeout: 300,
+
+      persons: [],
+      error: null
     };
+  }
+
+
+  componentDidMount() {
+
+    axios.get("http://192.168.0.112:5000/test")
+      .then(result => {
+        const persons = result.data.dulieu;
+        this.setState({ persons });
+        console.log(persons);
+      })
+      .catch(error => console.log(error))
   }
 
   toggle() {
@@ -157,6 +245,9 @@ class Cards extends Component {
   toggleFade() {
     this.setState((prevState) => { return { fadeIn: !prevState } });
   }
+
+
+
 
   render() {
     return (
@@ -170,6 +261,14 @@ class Cards extends Component {
             <Neww5 />
           </Col>
         </Row>
+        <Row>
+          <Col xs="12" sm="6" md="4">
+            <Funcc persons={this.state.persons} />
+          </Col>
+
+        </Row>
+
+
       </div>
     );
   }
